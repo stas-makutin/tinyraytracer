728c9c6c56f1a499bfa2462c44f1941374dd3537
stas-makutin <stas@dealerscience.com>
2019-05-07 15:39:37 -0400

    update: ray intersection with triangle and plane

--- objects.go
+++ objects.go
@@ -49,29 +49,31 @@ type triangle struct {
 
 // Moller-Trumbore algorithm
 // https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection
-func (t triangle) rayIntersect(orig, dir vec3) (bool, float64) {
-	edge1 := t.p2.sub(t.p1)
-	edge2 := t.p3.sub(t.p1)
-	pvec := dir.cross(edge2)
-	det := edge1.dot(pvec)
+func rayTriangleIntersect(orig, dir, p, v1, v2 vec3) (bool, float64) {
+	pvec := dir.cross(v2)
+	det := v1.dot(pvec)
 	if det < 1e-5 {
 		return false, 0
 	}
 
-	tvec := orig.sub(t.p1)
+	tvec := orig.sub(p)
 	u := tvec.dot(pvec)
 	if u < 0 || u > det {
 		return false, 0
 	}
 
-	qvec := tvec.cross(edge1)
+	qvec := tvec.cross(v1)
 	v := dir.dot(qvec)
 	if v < 0 || u+v > det {
 		return false, 0
 	}
 
-	tnear := edge2.dot(qvec) * (1 / det)
+	tnear := v2.dot(qvec) * (1 / det)
 	return tnear > 1e-5, tnear
+}
+
+func (t triangle) rayIntersect(orig, dir vec3) (bool, float64) {
+	return rayTriangleIntersect(orig, dir, t.p1, t.p2.sub(t.p1), t.p3.sub(t.p1))
 }
 
 func (t triangle) normal(_ vec3) vec3 {
@@ -87,15 +89,40 @@ type plane struct {
 	material  material
 }
 
+/*
+// https://stackoverflow.com/questions/21114796/3d-ray-quad-intersection-test-in-java
 func (p plane) rayIntersect(orig, dir vec3) (bool, float64) {
-	p2 := p.p.add(p.v1)
-	p3 := p.p.add(p.v2)
-	intersect, distance := triangle{p.p, p2, p3, nil}.rayIntersect(orig, dir)
+	n := p.v1.cross(p.v2).normalize()
+	nd := n.dot(dir)
+	if nd > -1e-6 && nd < 1e-6 {
+		return false, 0
+	}
+
+	t := -n.dot(orig.sub(p.p)) / nd
+	m := orig.add(dir.mul(t))
+
+	mp := m.sub(p.p)
+
+	u := mp.dot(p.v1)
+	if u < 0 || u > p.v1.dot(p.v1) {
+		return false, 0
+	}
+	v := mp.dot(p.v2)
+	if v < 0 || v > p.v2.dot(p.v2) {
+		return false, 0
+	}
+
+	return true, t
+}
+*/
+
+func (p plane) rayIntersect(orig, dir vec3) (bool, float64) {
+	intersect, distance := rayTriangleIntersect(orig, dir, p.p, p.v1, p.v2)
 	if intersect {
 		return true, distance
 	}
-	p4 := p3.add(p.v1)
-	return triangle{p2, p4, p3, nil}.rayIntersect(orig, dir)
+	p4 := p.p.add(p.v2).add(p.v1)
+	return rayTriangleIntersect(orig, dir, p4, p.v1.inv(), p.v2.inv())
 }
 
 func (p plane) normal(hit vec3) vec3 {
